bdfc9d54a897824d1ce65dbe108b2592bcbd7f97
Matt Muro <seriousfunk@users.noreply.github.com>
2018-05-16 21:21:02 -0400

    Update makesymlinks.sh
    add msg to update .gitconfig if not me.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -31,3 +31,5 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo "\e[0;33mUpdate your ~/.gitconfig if you are not Matt Muro\e[m"
