b88960344721560b4b8bc454b58da08075daacfa
Matt Muro <git@watchcity.com>
2018-05-30 13:32:58 -0400

    restoring files

--- uninstall.sh
+++ uninstall.sh
@@ -48,7 +48,7 @@ then
   cd $olddir  
   # copying any backed up dotfiles to users home directory
   for file in $files; do                                                                                             
-    cp $olddir/.$file ~                                                                                           
+    mv $olddir/.$file ~                                                                                           
   done
   echo "Restored your backed up dotfiles to your home directory."
 else
