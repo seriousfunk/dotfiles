3c012328df065239db4c0d5ced92026daad24378
Matt Muro <git@watchcity.com>
2018-05-30 11:40:32 -0400

    updating uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -12,52 +12,54 @@ olddir=~/dotfiles_old                         # old dotfiles backup directory
 files="bashrc vimrc vim gitconfig gitignore"  # list of files/folders to symlink in homedir                        
                                                                                                                    
 ##########                                                                                                         
-echo -e "\e[0;33m------------------------------------------------\e[m"
-echo "This script will delete the softlinks in your home directory to the"
-echo "dotfiles you installed from https://github.com/seriousfunk/dotfiles.
-echo -e "\e[0;33m------------------------------------------------\e[m"
+echo -e "\e[0;33m---------------------------------------------\e[m"
+echo "This script will the custom dotfiles that you "
+echo "installed from https://github.com/seriousfunk/dotfiles."
+echo -e "\e[0;33m---------------------------------------------\e[m"
 echo
 echo "Specifically ~/.bashrc ~/.gitconfig ~/.gitignore ~/.vim ~/.vimrc"
-echo 
-echo "This script will restore any original files backed up to dotfiles_old.
-echo 
-echo "\e[0;33mWould you like to proceed?\e[m"
 
 # confirm user wants to delete custom dotfiles and restore their files webacked up
-read -p "\e[0;33mWould you like to proceed?\e[m [Yn]" uninstallAnswer
+echo
+echo -e "\e[0;33mWould you like to proceed?\e[m [Yn]" 
+read uninstallAnswer
 ans=${uninstallAnswer:-Y}
 
 # if user answers n or N do not make any changes
 if [ n == $ans ] || [ N == $ans ]
 then
+  echo
   echo -e "No changes made."
+  echo
   exit 0
 fi
 
 # remove dotfiles from home directory
+echo
 echo -n "Removing ~/.bashrc ~/.gitconfig ~/.gitignore ~/.vim ~/.vimrc"        
 rm -f ~/.bashrc ~/.gitconfig ~/.gitignore ~/.vim ~/.vimrc 
-                                                                                                           
+echo                                                                                                        
+
 # check if user had files we backed up
 files=$(shopt -s nullglob dotglob; echo $olddir/*)
 if (( ${#files} ))
 then
   # change to the dotfiles_old directory where we backed things up                                                                                
-  echo -n "Changing to the $olddir directory ..."                                                                       
   cd $olddir  
   # copying any backed up dotfiles to users home directory
   for file in $files; do                                                                                             
-    echo "Copying any backed up dotfiles from $olddir to ~"                                                          
     cp $olddir/.$file ~                                                                                           
   done
-  echo -n "Restored your backed up dotfiles to your home directory."
-else 
-  echo -n "You did not have any dotfiles to back up so there is nothing to restore."
+  echo "Restored your backed up dotfiles to your home directory."
+else
+  echo "You did not have any dotfiles to restore."
 fi
 
+echo
 echo -n "Removing $dir and $olddir"
-rm -f $dir $olddir      
+rm -rf $dir $olddir      
 
-echo -n "Custom dotfiles removed. Everything set back to what it was before."     
-echo -n "done.                                                                                                        
+echo
+echo -e "\e[0;32Custom dotfiles removed. Everything set back to what it was before.\e[m"     
+echo "done.                                                                                                        
 
