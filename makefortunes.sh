04ee897b19cfe3290c44be989aa0d7d3720a34eb
Matt Muro <seriousfunk@users.noreply.github.com>
2018-09-11 16:07:48 -0400

    check to make sure fortune is installed

--- makefortunes.sh
+++ makefortunes.sh
@@ -5,21 +5,27 @@
 # Script origin: https://github.com/seriousfunk/dotfiles                                                                                                                     
 ############################                                                                            
                                                                                                    
-myFortunesDir=~/dotfiles/fortunes                                                                  
-fortunesDir=/usr/share/games/fortunes                                                              
-                                                                                                   
-echo ""                                                                                            
-echo -e "\e[0;96mCreating symlink of files in $myFortunesDir to $fortunesDir.\e[m"                 
-echo ""                                                                                            
-                                                                                                   
-for filename in $myFortunesDir/*; do                                                               
-  if [ -e $fortunesDir/${filename##*/} ]                                                           
-  then                                                                                             
-    echo -e "\e[0;31m-> $fortunesDir/${filename##*/} already exists. Not creating symlink.\e[m"    
-  else                                                                                             
-    ln -s $filename $fortunesDir/${filename##*/}                                                   
-    echo -e "\e[0;33m-> created symlink for $filename\e[m"                                         
-  fi                                                                                               
-done                                                                                               
-                                                                                                   
-echo ""                                                                                            
+# make sure fortune is installed                                                                 
+if [ -x /usr/games/fortune ]                                                                     
+then                                                                                             
+  echo "";                                                                                       
+  echo -e "\e[0;96mCreating symlink of files in $myFortunesDir to $fortunesDir.\e[m";            
+  echo "";                                                                                       
+else                                                                                             
+    echo "";                                                                                     
+    echo -e "\e[0;31mYou must install fortune first.\e[m";                                       
+    echo "";                                                                                     
+    exit 1;                                                                                      
+fi                                                                                               
+                                                                                                 
+for filename in $myFortunesDir/*; do                                                             
+  if [ -e $fortunesDir/${filename##*/} ]                                                         
+  then                                                                                           
+    echo -e "\e[0;31m-> $fortunesDir/${filename##*/} already exists. Not creating symlink.\e[m"  
+  else                                                                                           
+    ln -s $filename $fortunesDir/${filename##*/}                                                 
+    echo -e "\e[0;33m-> created symlink for $filename\e[m"                                       
+  fi                                                                                             
+done                                                                                             
+                                                                                                 
+echo ""                                                                                                                                                                                   
